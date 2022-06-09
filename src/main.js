7bae9d45c1e056e30ee1a32e7917df2ecf615a71
tuan <nguyentrungtuan15@gmail.com>
2022-06-09 08:29:54 +0700

    ed

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button';
+import AutoComplete from 'primevue/autocomplete';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import SpeedDial from 'primevue/speeddial';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                //core css
+import 'primeicons/primeicons.css'      
+import '/node_modules/primeflex/primeflex.css'
+const app = createApp(App);
+app.use(PrimeVue)
+
+app.component('Button', Button)
+app.component('AutoComplete',AutoComplete)
+app.component('SpeedDial',SpeedDial)
+app.component('InputText',InputText)
+app.component('Password',Password)
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/AboutView.vue
@@ -0,0 +1,74 @@
+
+
+<template>
+  <div class="about flex flex-column  ">
+    <h1>This is an about page</h1>
+
+
+      <h5 class="font-bold">Tên công ty</h5>
+        <div class="grid  p-fluid">
+             <div class="col-12 md:col-12 ">
+              <InputText class="border-round-2xl" placeholder="Tên" />
+            </div>
+        </div>
+
+        <h5 class="font-bold">Người đại diện</h5>
+        <div class="grid  p-fluid">
+             <div class="col-12 md:col-12 ">
+              <InputText class="border-round-2xl" placeholder="Tên" />
+            </div>
+        </div>
+        <h5 class="font-bold">Số điện thoại</h5>
+        <div class="grid  p-fluid">
+             <div class="col-12 md:col-12 ">
+              <InputText class="border-round-2xl" placeholder="Số điện thoại" />
+            </div>
+        </div>
+         
+        <div class="pass w-full">
+          <h5 class="font-bold">Nhâp Lại mật khẩu</h5>
+       <Password :class="'w-full'" v-bind:input-class="'border-round-2xl w-full'" v-model="oldPass" toggleMask placeholder="Mật khẩu"></Password>
+       </div>
+       <div>
+          <h5 class="font-bold">Nhâp Lại mật khẩu</h5>
+       <Password :class="'w-full'" v-bind:input-class=" 'border-round-2xl  w-full'" v-model="newPass" toggleMask placeholder="Mật khẩu"></Password>
+       </div>
+       <div>
+          <h5 class="font-bold">Nhâp Lại mật khẩu</h5>
+       <Password :class="'w-full'" v-bind:input-class="'border-round-2xl w-full'" v-model="reNewPass" toggleMask placeholder="Mật khẩu"></Password>
+       </div>
+       
+  </div>
+
+  
+</template>
+
+<script>
+
+
+export default{
+  data() {
+    return {
+      name: 'abc',
+      count: 0,
+      oldPass:'',
+      newPass:'',
+      reNewPass: ''
+    }
+  }
+}
+
+
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    
+    
+    
+  }
+}
+
+</style>
